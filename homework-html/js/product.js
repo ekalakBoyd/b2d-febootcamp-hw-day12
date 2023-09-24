fb654c1a6d80110035adcb3c1a45dfb0e72a547d
Boyd <ekalak.boyd@gmail.com>
2023-09-24 12:45:09 +0700

    original file add
    first commit

---
+++ homework-html/js/product.js
@@ -0,0 +1,4 @@
+const products = [
+  { cover: "./images/cover/cover1.jpg", name: "Stamp - Ekamai Dream 2 1Lp New", price: "1,890.00" },
+  { cover: "./images/cover/cover2.jpg", name: "", price: "" },
+];
